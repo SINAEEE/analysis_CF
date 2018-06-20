c69475a97eccc8527fbae0130d3a3e07528a7eba
SINAEEE <kimshin1227@naver.com>
2018-06-20 10:23:26 +0900

    crawling 함수구현

--- __main__.py
+++ __main__.py
@@ -1,11 +1,24 @@
 
 import collection.crawler as cw
 
-def my_error(e):
-    print("my error : " +e)
+#def my_error(e):
+#    print("my error : " + str(e))
+
+def proc(html):
+    print("processing..... " + html)
+
+def store(result):
+    pass
+
+
 
 result = cw.crawling(
     url='http://movie.naver.com/movie/sdb/rank/rmovie.nhn',
-    encoding='cp949')
+    encoding='cp949',
+    proc=proc,
+    store=store)
+    #err=my_error)
 
 print(result)
+#print("processing..."+result)
+
--- collection/crawler.py
+++ collection/crawler.py
@@ -3,18 +3,30 @@ import sys
 from urllib.request import Request,urlopen
 from datetime import datetime
 
+#def error(e):
+#    print('%s : %s' % (e.datetime.now()),file=sys.stderr)
 
 
 def crawling(
         url='',
-        encoding = 'utf-8'):
+        encoding = 'utf-8',
+        proc = lambda html: html, #통과코드
+        store=lambda html: html,
+        err=lambda e: print('%s : %s' % (e.datetime.now()),file=sys.stderr)):
     try:
         request = Request(url)
         resp = urlopen(request)
 
         try:
             receive = resp.read()
-            result = receive.decode(encoding)
+            result = receive.decode(encoding) #리시브에 디코드된 결과를 result에
+            result = store(proc(result)) #proc으로 html 리턴
+            """
+            if store is not None: #store = None일떄
+                result = store(result)
+            if proc is not None:
+                result = proc(result)
+            """
         except UnicodeDecodeError:
             result = receive.decode(encoding,'replace')
 
@@ -22,4 +34,5 @@ def crawling(
 
 
     except Exception as e:
-        print('%s : %s' % (e.datetime.now()),file=sys.stderr)
+        err(e)
+        #print('%s : %s' % (e.datetime.now()),file=sys.stderr)
